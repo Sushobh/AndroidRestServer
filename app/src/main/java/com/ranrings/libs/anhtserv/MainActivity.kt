dd7aa9e585b58c4ea33145745c818b5e0d88e1fe
Sushobh Nadiger <aryansushobh@gmail.com>
2020-01-14 22:51:07 +0530

    Added example

--- app/src/main/java/com/ranrings/libs/anhtserv/MainActivity.kt
+++ app/src/main/java/com/ranrings/libs/anhtserv/MainActivity.kt
@@ -2,11 +2,17 @@ package com.ranrings.libs.anhtserv
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.tv_message).text = """
+            Server is running on port ${MyApplication.application.port},
+            run this command on 'adb forward tcp:${MyApplication.application.port} tcp:[your_computer_port]'
+        """.trimIndent()
     }
 }
--- app/src/main/java/com/ranrings/libs/anhtserv/MyApplication.kt
+++ app/src/main/java/com/ranrings/libs/anhtserv/MyApplication.kt
@@ -4,73 +4,40 @@ import android.app.ActivityManager
 import android.app.Application
 import android.content.Context
 import com.ranrings.libs.androidapptorest.AndroidRestServer
-<<<<<<< HEAD
-import com.ranrings.libs.androidapptorest.GetRequestHandler
-=======
+
 import com.ranrings.libs.androidapptorest.Base.GetRequestHandler
 import com.ranrings.libs.androidapptorest.Base.PostRequestHandler
 import com.ranrings.libs.androidapptorest.Base.RequestHandler
->>>>>>> acc42f007044333c839496a4ae409fe766cbb9b9
 import org.json.JSONObject
 
 
 
 class MyApplication : Application() {
 
+    val port = 8080
+
+    companion object {
+        lateinit var application: MyApplication
+    }
+
     override fun onCreate() {
         super.onCreate()
-
+        application = this
         val androidRestServer = AndroidRestServer.Builder()
             .setApplication(this)
-            .setPort(8080)
-<<<<<<< HEAD
-            .addRequestHandler(object : GetRequestHandler() {
+            .setPort(port)
+            .addRequestHandler(object : GetRequestHandler<Any>(){
                 override fun onGetRequest(uri: String): Any {
-=======
-            .addRequestHandler(object : PostRequestHandler<Person, Any>(Person::class) {
+                    return packageName
+                }
+
                 override fun getMethodName(): String {
                     return "getpackagename"
                 }
 
-                override fun onRequest(requestBody: Person): Any {
->>>>>>> acc42f007044333c839496a4ae409fe766cbb9b9
-                    return JSONObject().apply {
-                        put("packagename",this@MyApplication.packageName)
-                    }
-                }
-<<<<<<< HEAD
-                override fun getMethodName(): String {
-                    return "getpackagename"
-=======
+            }).startWebApp(true).build()
 
-            }).addRequestHandler(object : PostRequestHandler<Person, Any>(Person::class) {
-                override fun getMethodName(): String {
-                    return "getcurrentactivity"
-                }
-
-                override fun onRequest(requestBody: Person): Any {
-                    val am =
-                        getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
-                    val cn = am.getRunningTasks(1)[0].topActivity
-                    return JSONObject().apply {
-                        put("packagename",cn.className)
-                    }
-                }
-
-            })
-            .addRequestHandler(object : GetRequestHandler<Any>() {
-                override fun onGetRequest(uri: String): Any {
-                    return "At best"
-                }
-
-                override fun getMethodName(): String {
-                    return "feel"
->>>>>>> acc42f007044333c839496a4ae409fe766cbb9b9
-                }
-            })
-            .
-                startWebApp(true).
-                build()
         androidRestServer.start()
+
     }
 }
